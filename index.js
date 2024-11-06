a1214870bf452cfdd358b671006b2cbbafb35d6b
KrishnaSunkara-30 <krishnasunkaragit@gmail.com>
2024-11-06 17:01:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// .features {
+//     display: flex;
+//     justify-content: space-evenly; /* Evenly spaces each feature across the container */
+//     align-items: center; /* Centers the items vertically */
+//     padding: 20px;
+//     /*background-color: #f9f9f9;  Optional background color */
+//   }
+
+//   .feature {
+//     text-align: center; /* Center-aligns text within each feature */
+//     max-width: 200px; /* Limits the width of each feature block */
+//   }
+
+//   .feature img {
+//     width: 100px; /* Adjusts the image size as desired */
+//     height: auto;
+//     margin-bottom: 10px;
+//   }
+
+//   .feature h3 {
+//     font-size: 1.2em;
+//     margin: 10px 0;
+//   }
+
+//   .feature p {
+//     color: #777;
+//     font-size: 0.9em;
+//   }
